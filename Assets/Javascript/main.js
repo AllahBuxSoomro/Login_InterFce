66f4a4dea714cea802c053c6e01f82060e7d1b02
Allah Bux Soomro <78809560+AllahBuxSoomro@users.noreply.github.com>
2021-06-28 23:45:12 +0500

    updated

---
+++ Assets/Javascript/main.js
@@ -0,0 +1,14 @@
+let btnl = document.querySelector("#login");
+let btnr = document.querySelector("#register");
+let z = document.querySelector("#btn");
+function register() {
+    btnl.style.left = "-400px";
+    btnr.style.left = "50px";
+    btnz.style.left = "110px";
+}
+
+function login() {
+    btnl.style.left = "50px";
+    btnr.style.left = "450px";
+    btnz.style.left = "0px";
+}
